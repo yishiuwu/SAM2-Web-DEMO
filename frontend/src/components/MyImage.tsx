32c21e73aa58db74629dfe8c3341d58e0518c29a
yishiuwu <94129760+yishiuwu@users.noreply.github.com>
2024-10-19 02:18:45 +0800

    click trace

---
+++ frontend/src/components/MyImage.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { MouseEvent, MouseEventHandler } from "react";
+
+interface ImageProps {
+    image_src: string;
+    handle_click: ()=>void;
+}
+export default function MyImage(props:ImageProps) {
+
+    const handleClick = (e:MouseEvent<HTMLImageElement>) => {
+        var rect = e.currentTarget.getBoundingClientRect();
+        var x = e.clientX - rect.left; //x position within the element.
+        var y = e.clientY - rect.top;  //y position within the element.
+        console.log("Left? : " + x + " ; Top? : " + y + ".");
+        
+    }
+
+    return (
+        <div className='h-[75vh] flex justify-center items-center mb-4'>
+            <img src={props.image_src} onClick={handleClick} className='max-h-full max-w-full object-contain p-2'/>
+        </div>
+    )
+    
+}
